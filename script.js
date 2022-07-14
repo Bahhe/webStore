be0535fdc202562e6d5e88f0059d7d2b1180671c
Bahhe <marchelldteach@gmail.com>
2022-07-14 03:13:41 +0100

    added slider

--- script.js
+++ script.js
@@ -11,36 +11,75 @@ function showSearchBar() {
   navList.classList.toggle('search-active')
 }
 
+// ADDING ITEMS DYNAMICALLLY
+
+// BOXES
+
+
+
+for(let i = 1; i <= 10; i++) {
+
+  const boxParents = document.querySelectorAll('.products-boxes')
+  boxParents.forEach(boxParent => {
+    const boxContainer = document.createElement('div')
+    boxContainer.classList.add('box-container')
+    boxContainer.innerHTML = `
+      <div class="box" style="background-image: url(img/products${i}.jpg);">
+                </div>
+                <div class="sub-box">
+                  <h2 class="product-title">product</h2>
+                  <p class="product-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, non repudiandae dolorem ipsum explicabo nesciunt odio est aliquid distinctio atque!</p>
+                  <button class="view">View</button>
+                </div>
+    `
+    boxParent.appendChild(boxContainer)
+  })
+}
+
+// RAWS
+
+
+for(let i = 1; i < 10; i++) {
+  const rawParents = document.querySelector('.new-arrivals')
+  const rawContainer = document.createElement('div')
+  rawContainer.classList.add('raws-container')
+  rawContainer.innerHTML = `
+    <div class="raws-container">
+      <div class="raws" style="background-image: url(img/products${i}.jpg);">
+      </div>
+      <div class="sub-raw">
+        <h2 class="product-title">product</h2>
+        <p class="product-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore hic, natus dolorem nulla voluptatibus, sed, quas blanditiis obcaecati nisi saepe reiciendis fugiat et modi soluta accusantium id numquam iusto deleniti.</p>
+        <div class="btn-container">
+          <button class="view">View</button>
+        </div>
+      </div>
+    </div>
+  `
+  rawParents.appendChild(rawContainer)
+}
 
 // SLIDER CODE 
 
-const sliderArrowLeft = document.querySelector('.slide-left')
-const sliderArrowRight = document.querySelector('.slide-right')
-const sliderItems = document.querySelector('.box-container')
-const slider = document.querySelectorAll('.products-boxes.slide .box-container')
+const slideContainer = [...document.querySelectorAll('.products-boxes.slide')]
+const sliderItems = [...document.querySelectorAll('.box-container')]
 
-slider.forEach((slid) => {
-  sliderArrowLeft.addEventListener('click', leftSlide)
 
-  let leftSlideCount = 1 
 
-  function leftSlide() {
-    const itemWidth = sliderItems.getBoundingClientRect().width
-    console.log(itemWidth)
-    slid.style.transform = `translateX(${leftSlideCount * itemWidth}px)`
-    leftSlideCount++
-  }
 
-  
-  sliderArrowRight.addEventListener('click', rightSlide)
 
-  let rightSlideCount = 1 
+const sliderArrowLeft = document.querySelector('.slide-left') 
+const sliderArrowRight = document.querySelector('.slide-right') 
+const slider = [...document.querySelectorAll('.products-boxes.slide .box-container')]
 
-  function rightSlide() {
-    const itemWidth = sliderItems.getBoundingClientRect().width
-    console.log(itemWidth)
-    slid.style.transform = `translateX(-${rightSlideCount * itemWidth}px)`
-    rightSlideCount++
-  }
+
+slideContainer.forEach((item, idx) => {
+  const width = item.getBoundingClientRect().width
+  sliderArrowRight.addEventListener('click', () => {
+    item.scrollLeft += width 
+  })
+
+  sliderArrowLeft.addEventListener('click', () => {
+    item.scrollLeft -= width 
+  })
 })
-
